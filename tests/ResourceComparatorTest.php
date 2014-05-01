0f5e7461663de63f57341d10ae95f9dabae9fd71
Jeff Welch <whatthejeff@gmail.com>
2014-05-01 01:14:49 -0400

    Added basic tests for some of the comparators.

---
+++ tests/ResourceComparatorTest.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * Comparator
+ *
+ * Copyright (c) 2001-2014, Sebastian Bergmann <sebastian@phpunit.de>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Sebastian Bergmann nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Comparator
+ * @author     Jeff Welch <whatthejeff@gmail.com>
+ * @copyright  2001-2014 Sebastian Bergmann <sebastian@phpunit.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://www.github.com/sebastianbergmann/comparator
+ */
+
+namespace SebastianBergmann\Comparator;
+
+/**
+ * @coversDefaultClass SebastianBergmann\Comparator\ResourceComparator
+ *
+ * @package    Comparator
+ * @author     Jeff Welch <whatthejeff@gmail.com>
+ * @copyright  2001-2014 Sebastian Bergmann <sebastian@phpunit.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://www.github.com/sebastianbergmann/comparator
+ */
+class ResourceComparatorTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->comparator = new ResourceComparator();
+    }
+
+    public function acceptsSucceedsProvider()
+    {
+        $tmpfile1 = tmpfile();
+        $tmpfile2 = tmpfile();
+
+        return array(
+          array($tmpfile1, $tmpfile1),
+          array($tmpfile2, $tmpfile2),
+          array($tmpfile1, $tmpfile2)
+        );
+    }
+
+    public function acceptsFailsProvider()
+    {
+        $tmpfile1 = tmpfile();
+
+        return array(
+          array($tmpfile1, null),
+          array(null, $tmpfile1),
+          array(null, null)
+        );
+    }
+
+    public function assertEqualsSucceedsProvider()
+    {
+        $tmpfile1 = tmpfile();
+        $tmpfile2 = tmpfile();
+
+        return array(
+          array($tmpfile1, $tmpfile1),
+          array($tmpfile2, $tmpfile2)
+        );
+    }
+
+    public function assertEqualsFailsProvider()
+    {
+        $tmpfile1 = tmpfile();
+        $tmpfile2 = tmpfile();
+
+        return array(
+          array($tmpfile1, $tmpfile2),
+          array($tmpfile2, $tmpfile1)
+        );
+    }
+
+    /**
+     * @covers       ::accepts
+     * @dataProvider acceptsSucceedsProvider
+     */
+    public function testAcceptsSucceeds($expected, $actual)
+    {
+        $this->assertTrue(
+          $this->comparator->accepts($expected, $actual)
+        );
+    }
+
+    /**
+     * @covers       ::accepts
+     * @dataProvider acceptsFailsProvider
+     */
+    public function testAcceptsFails($expected, $actual)
+    {
+        $this->assertFalse(
+          $this->comparator->accepts($expected, $actual)
+        );
+    }
+
+    /**
+     * @covers       ::assertEquals
+     * @dataProvider assertEqualsSucceedsProvider
+     */
+    public function testAssertEqualsSucceeds($expected, $actual)
+    {
+        $exception = null;
+
+        try {
+            $this->comparator->assertEquals($expected, $actual);
+        }
+
+        catch (ComparisonFailure $exception) {
+        }
+
+        $this->assertNull($exception, 'Unexpected ComparisonFailure');
+    }
+
+    /**
+     * @covers       ::assertEquals
+     * @dataProvider assertEqualsFailsProvider
+     */
+    public function testAssertEqualsFails($expected, $actual)
+    {
+        $this->setExpectedException('SebastianBergmann\\Comparator\\ComparisonFailure');
+        $this->comparator->assertEquals($expected, $actual);
+    }
+}
---
+++ tests/ScalarComparatorTest.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * Comparator
+ *
+ * Copyright (c) 2001-2014, Sebastian Bergmann <sebastian@phpunit.de>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Sebastian Bergmann nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Comparator
+ * @author     Jeff Welch <whatthejeff@gmail.com>
+ * @copyright  2001-2014 Sebastian Bergmann <sebastian@phpunit.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://www.github.com/sebastianbergmann/comparator
+ */
+
+namespace SebastianBergmann\Comparator;
+
+/**
+ * @coversDefaultClass SebastianBergmann\Comparator\ScalarComparator
+ *
+ * @package    Comparator
+ * @author     Jeff Welch <whatthejeff@gmail.com>
+ * @copyright  2001-2014 Sebastian Bergmann <sebastian@phpunit.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://www.github.com/sebastianbergmann/comparator
+ */
+class ScalarComparatorTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->comparator = new ScalarComparator();
+    }
+
+    public function acceptsSucceedsProvider()
+    {
+        return array(
+          array("string", "string"),
+          array(new ClassWithToString, "string"),
+          array("string", new ClassWithToString),
+          array("string", null),
+          array(false, "string"),
+          array(false, true),
+          array(null, false),
+          array(null, null),
+          array("10", 10),
+          array("", false),
+          array("1", true),
+          array(1, true),
+          array(0, false),
+          array(0.1, "0.1")
+        );
+    }
+
+    public function acceptsFailsProvider()
+    {
+        return array(
+          array(array(), array()),
+          array("string", array()),
+          array(new ClassWithToString, new ClassWithToString),
+          array(false, new ClassWithToString),
+          array(tmpfile(), tmpfile())
+        );
+    }
+
+    public function assertEqualsSucceedsProvider()
+    {
+        return array(
+          array("string", "string"),
+          array(new ClassWithToString, new ClassWithToString),
+          array("string representation", new ClassWithToString),
+          array(new ClassWithToString, "string representation"),
+          array("string", "STRING", true),
+          array("STRING", "string", true),
+          array("String Representation", new ClassWithToString, true),
+          array(new ClassWithToString, "String Representation", true),
+          array("10", 10),
+          array("", false),
+          array("1", true),
+          array(1, true),
+          array(0, false),
+          array(0.1, "0.1"),
+          array(false, null),
+          array(false, false),
+          array(true, true),
+          array(null, null)
+        );
+    }
+
+    public function assertEqualsFailsProvider()
+    {
+        $stringException = 'Failed asserting that two strings are equal.';
+        $otherException = 'matches expected';
+
+        return array(
+          array("string", "other string", $stringException),
+          array("string", "STRING", $stringException),
+          array("STRING", "string", $stringException),
+          array("string", "other string", $stringException),
+          array(new ClassWithToString, "does not match", $otherException),
+          array("does not match", new ClassWithToString, $otherException),
+          array("10", 25, $otherException),
+          array("1", false, $otherException),
+          array("", true, $otherException),
+          array(false, true, $otherException),
+          array(true, false, $otherException),
+          array(null, true, $otherException),
+          array(0, true, $otherException)
+        );
+    }
+
+    /**
+     * @covers       ::accepts
+     * @dataProvider acceptsSucceedsProvider
+     */
+    public function testAcceptsSucceeds($expected, $actual)
+    {
+        $this->assertTrue(
+          $this->comparator->accepts($expected, $actual)
+        );
+    }
+
+    /**
+     * @covers       ::accepts
+     * @dataProvider acceptsFailsProvider
+     */
+    public function testAcceptsFails($expected, $actual)
+    {
+        $this->assertFalse(
+          $this->comparator->accepts($expected, $actual)
+        );
+    }
+
+    /**
+     * @covers       ::assertEquals
+     * @dataProvider assertEqualsSucceedsProvider
+     */
+    public function testAssertEqualsSucceeds($expected, $actual, $ignoreCase = false)
+    {
+        $exception = null;
+
+        try {
+            $this->comparator->assertEquals($expected, $actual, 0.0, false, $ignoreCase);
+        }
+
+        catch (ComparisonFailure $exception) {
+        }
+
+        $this->assertNull($exception, 'Unexpected ComparisonFailure');
+    }
+
+    /**
+     * @covers       ::assertEquals
+     * @dataProvider assertEqualsFailsProvider
+     */
+    public function testAssertEqualsFails($expected, $actual, $message)
+    {
+        $this->setExpectedException('SebastianBergmann\\Comparator\\ComparisonFailure', $message);
+        $this->comparator->assertEquals($expected, $actual);
+    }
+}
