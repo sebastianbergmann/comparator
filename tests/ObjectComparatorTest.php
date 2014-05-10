1036f14c12b9b0a2e1af11e3e2a75a7173d6b189
Jeff Welch <whatthejeff@gmail.com>
2014-05-10 04:46:08 -0400

    Ported `ObjectComparator` tests.

---
+++ tests/ObjectComparatorTest.php
@@ -0,0 +1,186 @@
+<?php
+/**
+ * Comparator
+ *
+ * Copyright (c) 2001-2014, Sebastian Bergmann <sebastian@phpunit.de>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Sebastian Bergmann nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Comparator
+ * @author     Jeff Welch <whatthejeff@gmail.com>
+ * @copyright  2001-2014 Sebastian Bergmann <sebastian@phpunit.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://www.github.com/sebastianbergmann/comparator
+ */
+
+namespace SebastianBergmann\Comparator;
+
+use stdClass;
+
+/**
+ * @coversDefaultClass SebastianBergmann\Comparator\ObjectComparator
+ *
+ * @package    Comparator
+ * @author     Jeff Welch <whatthejeff@gmail.com>
+ * @copyright  2001-2014 Sebastian Bergmann <sebastian@phpunit.de>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://www.github.com/sebastianbergmann/comparator
+ */
+class ObjectComparatorTest extends \PHPUnit_Framework_TestCase
+{
+    private $comparator;
+
+    protected function setUp()
+    {
+        $this->comparator = new ObjectComparator;
+        $this->comparator->setFactory(new Factory);
+    }
+
+    public function acceptsSucceedsProvider()
+    {
+        return array(
+          array(new TestClass, new TestClass),
+          array(new stdClass, new stdClass),
+          array(new stdClass, new TestClass)
+        );
+    }
+
+    public function acceptsFailsProvider()
+    {
+        return array(
+          array(new stdClass, null),
+          array(null, new stdClass),
+          array(null, null)
+        );
+    }
+
+    public function assertEqualsSucceedsProvider()
+    {
+        // cyclic dependencies
+        $book1 = new Book;
+        $book1->author = new Author('Terry Pratchett');
+        $book1->author->books[] = $book1;
+        $book2 = new Book;
+        $book2->author = new Author('Terry Pratchett');
+        $book2->author->books[] = $book2;
+
+        $object1 = new SampleClass(4, 8, 15);
+        $object2 = new SampleClass(4, 8, 15);
+
+        return array(
+          array($object1, $object1),
+          array($object1, $object2),
+          array($book1, $book1),
+          array($book1, $book2)
+        );
+    }
+
+    public function assertEqualsFailsProvider()
+    {
+        $typeMessage = 'is not instance of expected class';
+        $equalMessage = 'Failed asserting that two objects are equal.';
+
+        // cyclic dependencies
+        $book1 = new Book;
+        $book1->author = new Author('Terry Pratchett');
+        $book1->author->books[] = $book1;
+        $book2 = new Book;
+        $book2->author = new Author('Terry Pratch');
+        $book2->author->books[] = $book2;
+
+        $book3 = new Book;
+        $book3->author = 'Terry Pratchett';
+        $book4 = new stdClass;
+        $book4->author = 'Terry Pratchett';
+
+        $object1 = new SampleClass( 4,  8, 15);
+        $object2 = new SampleClass(16, 23, 42);
+
+        return array(
+          array(new SampleClass(4, 8, 15), new SampleClass(16, 23, 42), $equalMessage),
+          array($object1, $object2, $equalMessage),
+          array($book1, $book2, $equalMessage),
+          array($book3, $book4, $typeMessage)
+        );
+    }
+
+    /**
+     * @covers       ::accepts
+     * @dataProvider acceptsSucceedsProvider
+     */
+    public function testAcceptsSucceeds($expected, $actual)
+    {
+        $this->assertTrue(
+          $this->comparator->accepts($expected, $actual)
+        );
+    }
+
+    /**
+     * @covers       ::accepts
+     * @dataProvider acceptsFailsProvider
+     */
+    public function testAcceptsFails($expected, $actual)
+    {
+        $this->assertFalse(
+          $this->comparator->accepts($expected, $actual)
+        );
+    }
+
+    /**
+     * @covers       ::assertEquals
+     * @dataProvider assertEqualsSucceedsProvider
+     */
+    public function testAssertEqualsSucceeds($expected, $actual)
+    {
+        $exception = null;
+
+        try {
+            $this->comparator->assertEquals($expected, $actual);
+        }
+
+        catch (ComparisonFailure $exception) {
+        }
+
+        $this->assertNull($exception, 'Unexpected ComparisonFailure');
+    }
+
+    /**
+     * @covers       ::assertEquals
+     * @dataProvider assertEqualsFailsProvider
+     */
+    public function testAssertEqualsFails($expected, $actual, $message)
+    {
+        $this->setExpectedException(
+          'SebastianBergmann\\Comparator\\ComparisonFailure', $message
+        );
+        $this->comparator->assertEquals($expected, $actual);
+    }
+}
--- tests/autoload.php
+++ tests/autoload.php
@@ -7,10 +7,20 @@ spl_autoload_register(
         static $classes = null;
         if ($classes === null) {
             $classes = array(
+                'sebastianbergmann\\comparator\\author' => '/_files/Author.php',
+                'sebastianbergmann\\comparator\\book' => '/_files/Book.php',
                 'sebastianbergmann\\comparator\\classwithtostring' => '/_files/ClassWithToString.php',
+                'sebastianbergmann\\comparator\\datetimecomparatortest' => '/DateTimeComparatorTest.php',
+                'sebastianbergmann\\comparator\\exceptioncomparatortest' => '/ExceptionComparatorTest.php',
                 'sebastianbergmann\\comparator\\factorytest' => '/FactoryTest.php',
+                'sebastianbergmann\\comparator\\objectcomparatortest' => '/ObjectComparatorTest.php',
+                'sebastianbergmann\\comparator\\resourcecomparatortest' => '/ResourceComparatorTest.php',
+                'sebastianbergmann\\comparator\\sampleclass' => '/_files/SampleClass.php',
+                'sebastianbergmann\\comparator\\scalarcomparatortest' => '/ScalarComparatorTest.php',
+                'sebastianbergmann\\comparator\\splobjectstoragecomparatortest' => '/SplObjectStorageComparatorTest.php',
                 'sebastianbergmann\\comparator\\testclass' => '/_files/TestClass.php',
-                'sebastianbergmann\\comparator\\testclasscomparator' => '/_files/TestClassComparator.php'
+                'sebastianbergmann\\comparator\\testclasscomparator' => '/_files/TestClassComparator.php',
+                'sebastianbergmann\\comparator\\typecomparatortest' => '/TypeComparatorTest.php'
             );
         }
         $cn = strtolower($class);
