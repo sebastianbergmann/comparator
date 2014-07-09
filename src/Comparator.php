f4e336bdef51ce8e117d2518c70298ac661a0bce
TimiTao <kunicki.tomasz@gmail.com>
2014-07-09 21:32:36 +0200

    Fix inheritance of interface function

--- src/Comparator.php
+++ src/Comparator.php
@@ -79,30 +79,4 @@ abstract class Comparator implements ComparatorInterface
     {
         $this->factory = $factory;
     }
-
-    /**
-     * Returns whether the comparator can compare two values.
-     *
-     * @param  mixed $expected The first value to compare
-     * @param  mixed $actual The second value to compare
-     * @return boolean
-     */
-    abstract public function accepts($expected, $actual);
-
-    /**
-     * Asserts that two values are equal.
-     *
-     * @param  mixed $expected The first value to compare
-     * @param  mixed $actual The second value to compare
-     * @param  float $delta The allowed numerical distance between two values to
-     *                      consider them equal
-     * @param  bool  $canonicalize If set to TRUE, arrays are sorted before
-     *                             comparison
-     * @param  bool  $ignoreCase If set to TRUE, upper- and lowercasing is
-     *                           ignored when comparing string values
-     * @throws ComparisonFailure Thrown when the comparison
-     *                           fails. Contains information about the
-     *                           specific errors that lead to the failure.
-     */
-    abstract public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false);
 }
