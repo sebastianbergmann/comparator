674bc1aa8edce29d7795f76278058024b3d2f66a
Nicolò Pignatelli <pignatelli.nicolo@gmail.com>
2015-05-30 17:41:50 +0200

    Fixed comparing SplObjectStorage with equal elements throws exception

--- src/SplObjectStorageComparator.php
+++ src/SplObjectStorageComparator.php
@@ -50,21 +50,46 @@ class SplObjectStorageComparator extends Comparator
      */
     public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
     {
-        foreach ($actual as $object) {
-            if (!$expected->contains($object)) {
-                throw new ComparisonFailure(
-                    $expected,
-                    $actual,
-                    $this->exporter->export($expected),
-                    $this->exporter->export($actual),
-                    false,
-                    'Failed asserting that two objects are equal.'
-                );
-            }
+        if ($expected->count() != $actual->count()) {
+            throw new ComparisonFailure(
+                $expected,
+                $actual,
+                $this->exporter->export($expected),
+                $this->exporter->export($actual),
+                false,
+                'Failed asserting that two objects are equal.'
+            );
         }
 
-        foreach ($expected as $object) {
-            if (!$actual->contains($object)) {
+        $expected = clone $expected;
+        $actual = clone $actual;
+
+        foreach ($actual as $actualObject) {
+            $equal = false;
+
+            foreach ($expected as $expectedObject) {
+
+                try {
+                    $comparator = $this->factory->getComparatorFor($expectedObject, $actualObject);
+                    $comparator->assertEquals($expectedObject, $actualObject, $delta, $canonicalize, $ignoreCase);
+
+                    $actualObjectData = $actual[$actualObject];
+                    $expectedObjectData = $expected[$expectedObject];
+
+                    $comparator = $this->factory->getComparatorFor($expectedObjectData, $actualObjectData);
+                    $comparator->assertEquals($expectedObjectData, $actualObjectData, $delta, $canonicalize, $ignoreCase);
+                } catch (ComparisonFailure $e) {
+                    continue;
+                }
+
+                $expected->detach($expectedObject);
+                $expected->rewind();
+                $equal = true;
+
+                break;
+            }
+
+            if (!$equal) {
                 throw new ComparisonFailure(
                     $expected,
                     $actual,
--- tests/SplObjectStorageComparatorTest.php
+++ tests/SplObjectStorageComparatorTest.php
@@ -29,6 +29,7 @@ class SplObjectStorageComparatorTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $this->comparator = new SplObjectStorageComparator;
+        $this->comparator->setFactory(new Factory);
     }
 
     public function acceptsFailsProvider()
@@ -56,11 +57,25 @@ class SplObjectStorageComparatorTest extends \PHPUnit_Framework_TestCase
         $storage4->attach($object2);
         $storage4->attach($object1);
 
+        $storage5 = new SplObjectStorage();
+        $storage5->attach($object1);
+
+        $storage6 = new SplObjectStorage();
+        $storage6->attach($object2);
+
+        $storage7 = new SplObjectStorage();
+        $storage7->attach($object1, 'data');
+
+        $storage8 = new SplObjectStorage();
+        $storage8->attach($object2, 'data');
+
         return array(
           array($storage1, $storage1),
           array($storage1, $storage2),
           array($storage3, $storage3),
-          array($storage3, $storage4)
+          array($storage3, $storage4),
+          array($storage5, $storage6),
+          array($storage7, $storage8),
         );
     }
 
@@ -68,6 +83,12 @@ class SplObjectStorageComparatorTest extends \PHPUnit_Framework_TestCase
     {
         $object1 = new stdClass;
         $object2 = new stdClass;
+
+        $object3 = new stdClass;
+        $object3->name = 'object3';
+
+        $object4 = new stdClass;
+        $object4->name = 'object4';
 
         $storage1 = new SplObjectStorage;
 
@@ -78,10 +99,24 @@ class SplObjectStorageComparatorTest extends \PHPUnit_Framework_TestCase
         $storage3->attach($object2);
         $storage3->attach($object1);
 
+        $storage4 = new SplObjectStorage;
+        $storage4->attach($object3);
+
+        $storage5 = new SplObjectStorage;
+        $storage5->attach($object4);
+
+        $storage6 = new SplObjectStorage;
+        $storage6->attach($object1, 'data1');
+
+        $storage7 = new SplObjectStorage;
+        $storage7->attach($object1, 'data2');
+
         return array(
           array($storage1, $storage2),
           array($storage1, $storage3),
           array($storage2, $storage3),
+          array($storage4, $storage5),
+          array($storage6, $storage7),
         );
     }
 
