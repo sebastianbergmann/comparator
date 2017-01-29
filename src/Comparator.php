sebastianbergmann/comparator

commit 9d9ede5a25938443d697a92551d031e83ed62856

Make assertEquals declarations compatible

src/Comparator.php
@@ -64,5 +64,5 @@ abstract class Comparator
      *                                        fails. Contains information about the
      *                                        specific errors that lead to the failure.
      */
-    abstract public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false);
+    abstract public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array());
 }

src/DoubleComparator.php
@@ -49,7 +49,7 @@ class DoubleComparator extends NumericComparator
      *                                        fails. Contains information about the
      *                                        specific errors that lead to the failure.
      */
-    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array())
     {
         if ($delta == 0) {
             $delta = self::EPSILON;

src/NumericComparator.php
@@ -46,7 +46,7 @@ class NumericComparator extends ScalarComparator
      *                                        fails. Contains information about the
      *                                        specific errors that lead to the failure.
      */
-    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array())
     {
         if (is_infinite($actual) && is_infinite($expected)) {
             return;

src/ResourceComparator.php
@@ -42,7 +42,7 @@ class ResourceComparator extends Comparator
      *                                        fails. Contains information about the
      *                                        specific errors that lead to the failure.
      */
-    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array())
     {
         if ($actual != $expected) {
             throw new ComparisonFailure(

src/ScalarComparator.php
@@ -47,7 +47,7 @@ class ScalarComparator extends Comparator
      *                                        fails. Contains information about the
      *                                        specific errors that lead to the failure.
      */
-    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array())
     {
         $expectedToCompare = $expected;
         $actualToCompare   = $actual;

src/SplObjectStorageComparator.php
@@ -42,7 +42,7 @@ class SplObjectStorageComparator extends Comparator
      *                                        fails. Contains information about the
      *                                        specific errors that lead to the failure.
      */
-    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array())
     {
         foreach ($actual as $object) {
             if (!$expected->contains($object)) {

src/TypeComparator.php
@@ -42,7 +42,7 @@ class TypeComparator extends Comparator
      *                                        fails. Contains information about the
      *                                        specific errors that lead to the failure.
      */
-    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array())
     {
         if (gettype($expected) != gettype($actual)) {
             throw new ComparisonFailure(

src/DOMNodeComparator.php
@@ -45,7 +45,7 @@ class DOMNodeComparator extends ObjectComparator
      *                                        fails. Contains information about the
      *                                        specific errors that lead to the failure.
      */
-    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array())
     {
         $expectedAsString = $this->nodeToText($expected, true, $ignoreCase);
         $actualAsString   = $this->nodeToText($actual, true, $ignoreCase);

commit 2b7424b55f5047b47ac6e5ccb20b2aea4011d9be

Minimize backwards compatibility break introduced in 9d9ede5a25938443d697a92551d031e83ed62856

src/Comparator.php
@@ -57,9 +57,8 @@ abstract class Comparator
      * @param float $delta        Allowed numerical distance between two values to consider them equal
      * @param bool  $canonicalize Arrays are sorted before comparison when set to true
      * @param bool  $ignoreCase   Case is ignored when set to true
-     * @param array $processed    List of already processed elements (used to prevent infinite recursion)
      *
      * @throws ComparisonFailure
      */
-    abstract public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array());
+    abstract public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false);
 }

src/DoubleComparator.php
@@ -42,11 +42,10 @@ class DoubleComparator extends NumericComparator
      * @param float $delta        Allowed numerical distance between two values to consider them equal
      * @param bool  $canonicalize Arrays are sorted before comparison when set to true
      * @param bool  $ignoreCase   Case is ignored when set to true
-     * @param array $processed    List of already processed elements (used to prevent infinite recursion)
      *
      * @throws ComparisonFailure
      */
-    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array())
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
     {
         if ($delta == 0) {
             $delta = self::EPSILON;

src/NumericComparator.php
@@ -39,11 +39,10 @@ class NumericComparator extends ScalarComparator
      * @param float $delta        Allowed numerical distance between two values to consider them equal
      * @param bool  $canonicalize Arrays are sorted before comparison when set to true
      * @param bool  $ignoreCase   Case is ignored when set to true
-     * @param array $processed    List of already processed elements (used to prevent infinite recursion)
      *
      * @throws ComparisonFailure
      */
-    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array())
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
     {
         if (is_infinite($actual) && is_infinite($expected)) {
             return;

src/ResourceComparator.php
@@ -35,11 +35,10 @@ class ResourceComparator extends Comparator
      * @param float $delta        Allowed numerical distance between two values to consider them equal
      * @param bool  $canonicalize Arrays are sorted before comparison when set to true
      * @param bool  $ignoreCase   Case is ignored when set to true
-     * @param array $processed    List of already processed elements (used to prevent infinite recursion)
      *
      * @throws ComparisonFailure
      */
-    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array())
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
     {
         if ($actual != $expected) {
             throw new ComparisonFailure(

src/ScalarComparator.php
@@ -40,11 +40,10 @@ class ScalarComparator extends Comparator
      * @param float $delta        Allowed numerical distance between two values to consider them equal
      * @param bool  $canonicalize Arrays are sorted before comparison when set to true
      * @param bool  $ignoreCase   Case is ignored when set to true
-     * @param array $processed    List of already processed elements (used to prevent infinite recursion)
      *
      * @throws ComparisonFailure
      */
-    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array())
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
     {
         $expectedToCompare = $expected;
         $actualToCompare   = $actual;

src/SplObjectStorageComparator.php
@@ -35,11 +35,10 @@ class SplObjectStorageComparator extends Comparator
      * @param float $delta        Allowed numerical distance between two values to consider them equal
      * @param bool  $canonicalize Arrays are sorted before comparison when set to true
      * @param bool  $ignoreCase   Case is ignored when set to true
-     * @param array $processed    List of already processed elements (used to prevent infinite recursion)
      *
      * @throws ComparisonFailure
      */
-    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array())
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
     {
         foreach ($actual as $object) {
             if (!$expected->contains($object)) {

src/TypeComparator.php
@@ -35,11 +35,10 @@ class TypeComparator extends Comparator
      * @param float $delta        Allowed numerical distance between two values to consider them equal
      * @param bool  $canonicalize Arrays are sorted before comparison when set to true
      * @param bool  $ignoreCase   Case is ignored when set to true
-     * @param array $processed    List of already processed elements (used to prevent infinite recursion)
      *
      * @throws ComparisonFailure
      */
-    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array())
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
     {
         if (gettype($expected) != gettype($actual)) {
             throw new ComparisonFailure(
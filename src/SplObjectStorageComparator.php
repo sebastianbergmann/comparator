2b7424b55f5047b47ac6e5ccb20b2aea4011d9be
Sebastian Bergmann <sb@sebastian-bergmann.de>
2017-01-29 10:50:25 +0100

    Minimize backwards compatibility break introduced in 9d9ede5a25938443d697a92551d031e83ed62856

--- src/SplObjectStorageComparator.php
+++ src/SplObjectStorageComparator.php
@@ -35,11 +35,10 @@ class SplObjectStorageComparator extends Comparator
      * @param float $delta        Allowed numerical distance between two values to consider them equal
      * @param bool  $canonicalize Arrays are sorted before comparison when set to true
      * @param bool  $ignoreCase   Case is ignored when set to true
-     * @param array $processed    List of already processed elements (used to prevent infinite recursion)
      *
      * @throws ComparisonFailure
      */
-    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = array())
+    public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false)
     {
         foreach ($actual as $object) {
             if (!$expected->contains($object)) {
