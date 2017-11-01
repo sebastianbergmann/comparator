f805963ee2e7d38a474d6ebc8961b1854eed210a
Sebastian Bergmann <sb@sebastian-bergmann.de>
2017-11-01 09:33:06 +0100

    Be compatible with phpunit-mock-objects ^5.0

--- src/MockObjectComparator.php
+++ src/MockObjectComparator.php
@@ -25,7 +25,8 @@ class MockObjectComparator extends ObjectComparator
      */
     public function accepts($expected, $actual)
     {
-        return $expected instanceof \PHPUnit_Framework_MockObject_MockObject && $actual instanceof \PHPUnit_Framework_MockObject_MockObject;
+        return ($expected instanceof \PHPUnit_Framework_MockObject_MockObject || $expected instanceof \PHPUnit\Framework\MockObject\MockObject) &&
+               ($actual instanceof \PHPUnit_Framework_MockObject_MockObject || $actual instanceof \PHPUnit\Framework\MockObject\MockObject);
     }
 
     /**
