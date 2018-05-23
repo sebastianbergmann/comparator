133b441cf87710df90b25c55c5d73efdd2425cea
or <igor_deyawka@mail.ru>
2018-05-23 08:34:48 +0200

    DOMNodeComparator::nodeToText Refactoring
    1. I removed
    
        $document->formatOutput = true;
        $document->normalizeDocument();
    
    Because the unit-tests not failed after that and I don't see why it needed.
    2. I removed a lot of checks instanceof DOMDocument because in fact there is always DOMDocument in the $node variable in case if canonicalize is true (and it is always true).
    3. I removed $canonicalize because in fact it is always true and the unused argument just make the code maintenance harder. Especially without corresponding unit-tests.

--- src/DOMNodeComparator.php
+++ src/DOMNodeComparator.php
@@ -37,7 +37,7 @@ class DOMNodeComparator extends ObjectComparator
      * @param mixed $expected     First value to compare
      * @param mixed $actual       Second value to compare
      * @param float $delta        Allowed numerical distance between two values to consider them equal
-     * @param bool  $canonicalize Arrays are sorted before comparison when set to true
+     * @param bool  $canonicalize The value of this argument ignored and always considered as true
      * @param bool  $ignoreCase   Case is ignored when set to true
      * @param array $processed    List of already processed elements (used to prevent infinite recursion)
      *
@@ -45,8 +45,8 @@ class DOMNodeComparator extends ObjectComparator
      */
     public function assertEquals($expected, $actual, $delta = 0.0, $canonicalize = false, $ignoreCase = false, array &$processed = [])
     {
-        $expectedAsString = $this->nodeToText($expected, true, $ignoreCase);
-        $actualAsString   = $this->nodeToText($actual, true, $ignoreCase);
+        $expectedAsString = $this->nodeToText($expected, $ignoreCase);
+        $actualAsString   = $this->nodeToText($actual, $ignoreCase);
 
         if ($expectedAsString !== $actualAsString) {
             $type = $expected instanceof DOMDocument ? 'documents' : 'nodes';
@@ -63,24 +63,28 @@ class DOMNodeComparator extends ObjectComparator
     }
 
     /**
-     * Returns the normalized, whitespace-cleaned, and indented textual
-     * representation of a DOMNode.
+     * Returns the normalized, whitespace-cleaned, and indented textual representation of a DOMNode.
+     *
+     * @param  DOMNode $node
+     * @param  bool $ignoreCase If true - whole xml (tags and inner text) will be converted to lowercase during
+     *   comparison
+     *
+     * @return string Text representation of DOMNode
      */
-    private function nodeToText(DOMNode $node, bool $canonicalize, bool $ignoreCase): string
+    private function nodeToText(DOMNode $node, bool $ignoreCase): string
     {
-        if ($canonicalize) {
-            $document = new DOMDocument;
-            @$document->loadXML($node->C14N());
+        $document = new DOMDocument();
 
-            $node = $document;
+        $nodeString = $node->C14N();
+
+        // If an empty string is passed as the source, a warning will be generated. We prevent it here.
+        if ($nodeString !== "") {
+            $document->loadXML($nodeString);
         }
 
-        $document = $node instanceof DOMDocument ? $node : $node->ownerDocument;
+        $node = $document;
 
-        $document->formatOutput = true;
-        $document->normalizeDocument();
-
-        $text = $node instanceof DOMDocument ? $node->saveXML() : $document->saveXML($node);
+        $text = $node->saveXML();
 
         return $ignoreCase ? \strtolower($text) : $text;
     }
