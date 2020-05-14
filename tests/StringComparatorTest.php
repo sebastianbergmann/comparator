1e8f2ab6d0943eb1a850f0ebbc906fb5ccf1296d
Aleksandr Novikov <pwsdotru@gmail.com>
2020-05-14 20:19:13 +0300

    #82 Add Strings comparator and test for it

---
+++ tests/StringComparatorTest.php
@@ -0,0 +1,122 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of sebastian/comparator.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianBergmann\Comparator;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \SebastianBergmann\Comparator\StringComparator<extended>
+ *
+ * @uses \SebastianBergmann\Comparator\Comparator
+ * @uses \SebastianBergmann\Comparator\Factory
+ * @uses \SebastianBergmann\Comparator\ComparisonFailure
+ */
+final class StringComparatorTest extends TestCase
+{
+    /**
+     * @var ScalarComparator
+     */
+    private $comparator;
+
+    protected function setUp(): void
+    {
+        $this->comparator = new StringComparator;
+    }
+
+    public function acceptsSucceedsProvider()
+    {
+        return [
+            ['string', 'string'],
+            ['', 'String'],
+        ];
+    }
+
+    public function acceptsFailsProvider()
+    {
+        return [
+            [[], []],
+            ['string', []],
+            ['10', 10],
+            ['', false],
+            ['1', true],
+        ];
+    }
+
+    public function assertEqualsSucceedsProvider()
+    {
+        return [
+            ['string', 'string'],
+            ['string', 'STRING', true],
+            ['STRING', 'string', true],
+        ];
+    }
+
+    public function assertEqualsFailsProvider()
+    {
+        $stringException = 'Failed asserting that two strings are equal.';
+        $otherException  = 'matches expected';
+
+        return [
+            ['string', 'other string', $stringException],
+            ['string', 'STRING', $stringException],
+            ['STRING', 'string', $stringException],
+            ['string', 'other string', $stringException],
+            ['0', '0.0', $stringException],
+            ['0.', '0.0', $stringException],
+            ['0e1', '0e2', $stringException],
+        ];
+    }
+
+    /**
+     * @dataProvider acceptsSucceedsProvider
+     */
+    public function testAcceptsSucceeds($expected, $actual): void
+    {
+        $this->assertTrue(
+            $this->comparator->accepts($expected, $actual)
+        );
+    }
+
+    /**
+     * @dataProvider acceptsFailsProvider
+     */
+    public function testAcceptsFails($expected, $actual): void
+    {
+        $this->assertFalse(
+            $this->comparator->accepts($expected, $actual)
+        );
+    }
+
+    /**
+     * @dataProvider assertEqualsSucceedsProvider
+     */
+    public function testAssertEqualsSucceeds($expected, $actual, $ignoreCase = false): void
+    {
+        $exception = null;
+
+        try {
+            $this->comparator->assertEquals($expected, $actual, 0.0, false, $ignoreCase);
+        } catch (ComparisonFailure $exception) {
+        }
+
+        $this->assertNull($exception, 'Unexpected ComparisonFailure');
+    }
+
+    /**
+     * @dataProvider assertEqualsFailsProvider
+     */
+    public function testAssertEqualsFails($expected, $actual, $message): void
+    {
+        $this->expectException(ComparisonFailure::class);
+        $this->expectExceptionMessage($message);
+
+        $this->comparator->assertEquals($expected, $actual);
+    }
+}
