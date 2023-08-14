d5c5accdac10915627ab1fb06097abba0f4739d3
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-08-14 14:55:11 +0200

    Remove superfluous comments

--- src/MockObjectComparator.php
+++ src/MockObjectComparator.php
@@ -17,12 +17,6 @@ use PHPUnit\Framework\MockObject\MockObject;
  */
 final class MockObjectComparator extends ObjectComparator
 {
-    /**
-     * Returns whether the comparator can compare two values.
-     *
-     * @param mixed $expected The first value to compare
-     * @param mixed $actual   The second value to compare
-     */
     public function accepts(mixed $expected, mixed $actual): bool
     {
         return $expected instanceof MockObject && $actual instanceof MockObject;
