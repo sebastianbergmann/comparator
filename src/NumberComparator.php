d1466a875d4d67622acbc43f3463e96d5497249a
Sebastian Bergmann <sb@sebastian-bergmann.de>
2026-02-05 16:44:39 +0100

    Fix errors identified by PHPStan

--- src/NumberComparator.php
+++ src/NumberComparator.php
@@ -39,13 +39,13 @@ final class NumberComparator extends ObjectComparator
     public function assertEquals(mixed $expected, mixed $actual, float $delta = 0.0, bool $canonicalize = false, bool $ignoreCase = false, array &$processed = []): void
     {
         if (!$expected instanceof Number) {
-            assert(is_string($expected) || is_int($expected));
+            assert(is_int($expected) || is_string($expected) && is_numeric($expected));
 
             $expected = new Number($expected);
         }
 
         if (!$actual instanceof Number) {
-            assert(is_string($actual) || is_int($actual));
+            assert(is_int($actual) || is_string($actual) && is_numeric($actual));
 
             $actual = new Number($actual);
         }
